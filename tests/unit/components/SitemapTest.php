dec2b01ba4b6bd9bc5bedf5ee2576cbdfa131260
Ilya Zhelyabuzhsky <zhelyabuzhsky@gmail.com>
2015-06-04 17:17:50 +0500

    Intermediate commit

---
+++ tests/unit/components/SitemapTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace tests\unit\components;
+
+use zhelyabuzhsky\sitemap\components\Sitemap;
+use Yii;
+
+class ComponentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
